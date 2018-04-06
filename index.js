dc5f6fedb88ee7305a7d5708791a0a7b95140f08
Lonny Snyder <lonnysnyder81@gmail.com>
2018-04-06 09:27:29 -0500

    work in progress, added json user import

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var server = express();
 server.set('port',3000);
 server.use(express.static(__dirname));
+
 server.listen(server.get('port'),function(){
   console.log('Express server listening on port '+server.get('port'));
 });
--- users/userlistcomponent.js
+++ users/userlistcomponent.js
@@ -4,11 +4,19 @@ angular.
   module('userList').
     component('userList',{
       templateUrl: 'users/userfrag.html',
-      controller: function UserListController(){
-        this.users = [{name: 'Travis', password: "password", loggedIn:false},
+      controller: function UserListController($http){
+        var self =this;
+        this.random=function(){
+          var num = Math.random();
+          return num;
+        };
+        $http.get("users/users.json").then(function(response){
+          self.users = response.data;
+        });
+      /*  this.users = [{name: 'Travis', password: "password", loggedIn:false},
                      {name: 'Chris', password:"password", loggedIn:false},
                      {name: 'Lonny', password:"password", loggedIn:false},
-                     {name: 'Blake', password:"password", loggedIn:false}];
+                     {name: 'Blake', password:"password", loggedIn:false}];*/
       }
 
     });
